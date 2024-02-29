3d2fc5f11ffa8f3233f586d2c6c35bead9315613
ArcaneCap <118013926+ArcaneCap@users.noreply.github.com>
2024-02-29 16:41:38 +0000

    Added the sm2 algorithm function in main.py

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-import pandas as csv
+import pandas as pd
 from io_handling import csv_file_menu
+from sm2 import ask_question
 
-csv_file_menu()
+
+
+
+
+if __name__ == "__main__":
+    df = csv_file_menu()
+    df['Next Review Date'] = pd.to_datetime(df['Next Review Date'])  # Ensure correct datetime format
+    try:
+        if ask_question(df):
+            print("All cards reviewed for today!")
+        print("Saving progress...")
+        df.to_csv('flashcards.csv', index=False)
+        print("Progress saved. Exiting flashcards.")
+    except KeyboardInterrupt:
+        print("\nSession interrupted. Saving progress...")
+        df.to_csv('flashcards.csv', index=False)
+        print("Progress saved. Goodbye!")
